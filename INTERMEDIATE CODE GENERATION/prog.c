8654c4da62051d4e9329b55de5e0d3b2b7c88a9a
Amruta Anandan <76481299+amrutaanandan@users.noreply.github.com>
2024-01-29 15:23:46 +0530

    INTERMEDIATE CODE GENERATION

--- INTERMEDIATE CODE GENERATION/prog.c
+++ INTERMEDIATE CODE GENERATION/prog.c
@@ -1,3 +1,17 @@
+/*
+Input
+Enter a line: a+b=c*d-e/f-g-h
+
+Output
+ 3AC    Triples         Quadruple
+Z=c*d   (*, c, d)       (*, c, d, Z)
+Y=e/f   (/, e, f)       (/, e, f, Y)
+X=a+b   (+, a, b)       (+, a, b, X)
+W=Z-Y   (-, Z, Y)       (-, Z, Y, W)
+V=g-h   (-, g, h)       (-, g, h, V)
+U=W-V   (-, W, V)       (-, W, V, U)
+*/
+
 #include <stdio.h>
 #include <string.h>
 
@@ -8,13 +22,13 @@ int flag=0;
 int main() {
     printf("\nEnter a line: ");
     scanf("%s", line);
-    
+    printf("\n 3AC \t Triples  \t  Quadruple\n");
     while(strlen(line) > 3){
         flag=0;
         if(!flag){
             for(int i=0; i<strlen(line); i++){
                 if(line[i] == '/' || line[i] == '*'){
-                    printf("%c=%c%c%c\n", tempvar, line[i-1], line[i], line[i+1]);
+                    printf("%c=%c%c%c\t(%c, %c, %c)\t(%c, %c, %c, %c)\n", tempvar, line[i-1], line[i], line[i+1], line[i], line[i-1], line[i+1], line[i], line[i-1], line[i+1], tempvar);
                     line[i-1] = tempvar;
                     tempvar--;
                     
@@ -27,7 +41,7 @@ int main() {
             }
             for(int i=0; i<strlen(line); i++){
                 if(line[i] == '+' || line[i] == '-'){
-                    printf("%c=%c%c%c\n", tempvar, line[i-1], line[i], line[i+1]);
+                    printf("%c=%c%c%c\t(%c, %c, %c)\t(%c, %c, %c, %c)\n", tempvar, line[i-1], line[i], line[i+1], line[i], line[i-1], line[i+1], line[i], line[i-1], line[i+1], tempvar);
                     line[i-1] = tempvar;
                     tempvar--;
                     
